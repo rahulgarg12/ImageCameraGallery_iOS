70a099bac14990b99b6377e11b49159d3def6e5d
Rahul Garg <rahulgarg@Rahuls-MacBook.local>
2014-08-01 01:30:30 +0530

    Initial Commit

---
+++ Image/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Image
+//
+//  Created by Rahul Garg on 01/08/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Image/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Image
+//
+//  Created by Rahul Garg on 01/08/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
